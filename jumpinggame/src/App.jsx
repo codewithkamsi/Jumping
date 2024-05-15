d5ad0950a90df41f3f1ff54773dd665761966843
unknown <aguguakamsi@gmail.com>
2024-05-15 18:34:20 +0100

    added circles

---
+++ jumpinggame/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='gameboard w-full h-full bg-blue-600'>
+      <div className='smallroundbox'></div>
+    </div>
+  )
+}
